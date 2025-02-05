e5f8960543aa36732975a6343295ae2e49d25646
zhollandio <zscholland@gmail.com>
2025-02-05 16:34:40 +0000

    Add VM initialization script

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# init.sh - Bootstrap script for VM setup
+
+# Update package index
+echo "Updating package index..."
+sudo apt update
+
+set -e  # exit on error
+
+# install: make, python3.12-venv & tree
+echo "Installing required packages: make, python3.12-venv, tree"
+sudo apt install -y make python3.12-venv tree
+
+echo "Install complete."
