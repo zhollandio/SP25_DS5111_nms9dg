cd742d737f2497b127b1b4df7264c04100d9aaa8
Efrain Olivares <99214069+EfrainOlivaresUVA@users.noreply.github.com>
2025-04-02 00:30:23 -0700

    Update test_environment.py

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -4,6 +4,6 @@ import os
 def test_os():
     assert os.name == "posix", "Tests should only run on Linux"
 
-def test_python_version():
+def test_python_version(): # just a head's up that you'll need to change the test when you update the validation.yaml
     """Ensure Python version is 3.10 or newer."""
     assert sys.version_info >= (3, 10), "Python version must be 3.10 or newer"
